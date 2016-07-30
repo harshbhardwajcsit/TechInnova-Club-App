2c05fa7c398ed69468e768d515542faf175c4cc4
harshbhardwaj29 <harshbhardwajcsit@gmail.com>
2016-07-31 01:18:03 +0530

    project Completed

--- app/src/main/java/com/example/samsung/techinnova/TechInnova.java
+++ app/src/main/java/com/example/samsung/techinnova/TechInnova.java
@@ -43,7 +43,7 @@ public class TechInnova extends Activity implements View.OnClickListener {
                 break;
 
             case R.id.webdesign:
-                startActivity(new Intent( TechInnova.this,LiveNotification.class ));
+                startActivity(new Intent( TechInnova.this,WebDev.class ));
                 break;
 
 
